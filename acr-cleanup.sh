65e87291c73b869a734b67bf5c43c45c59cb3993
Vinícius Fernandes <vinaumpt@gmail.com>
2024-07-08 14:31:27 -0300

    Update acr-cleanup.sh
    Added bilingual comments to the Azure Container Registry image cleanup script
    
    - Added comments in both Portuguese and English for better clarity and understanding.
    - Provided detailed explanations for each section and function of the script.
    - Preserved original functionalities for loading variables, authenticating with Azure, and removing images.
    
    This commit improves code readability and facilitates future maintenance.

--- acr-cleanup.sh
+++ acr-cleanup.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-# Carrega as variáveis do arquivo .env
+# Carrega as variáveis do arquivo .env, se existir e se ServicePrincipalId não estiver definida
+# Loads the variables from the .env file if it exists and if ServicePrincipalId is not set
 if [ -f .env ] && [ -z "$ServicePrincipalId" ]; then
     export $(cat .env | grep -v '^#' | xargs) > /dev/null 2>&1
 fi
 
-# Função para remover uma imagem
+# Função para remover uma imagem do Azure Container Registry
+# Function to remove an image from the Azure Container Registry
 function remove_image {
     registryName="$1"
     imageName="$2"
     dryRun="$3"
 
     if [ "$dryRun" = "true" ]; then
-        echo "Would have deleted $imageName"
+        echo "Would have deleted $imageName"  # Simulação de remoção
+        # Dry run: Simulating deletion
     else
-        echo "Proceeding to delete image: $imageName"
+        echo "Proceeding to delete image: $imageName"  # Remoção real
+        # Proceeding to delete the image
         az acr repository delete --name "$registryName" --image "$imageName" --yes
     fi
 }
 
-# Verifica se as variáveis estão definidas
+# Verifica se as variáveis necessárias estão definidas
+# Checks if the required variables are set
 if [ -z "$ServicePrincipalId" ] && [ -z "$ServicePrincipalPass" ] && [ -z "$ServicePrincipalTenant" ]; then
     echo "As variáveis ServicePrincipalId, ServicePrincipalPass e ServicePrincipalTenant devem ser definidas."
+    # The variables ServicePrincipalId, ServicePrincipalPass, and ServicePrincipalTenant must be set
     echo "Execute o container Docker com o seguinte comando:"
+    # Run the Docker container with the following command
     echo ""
     echo "docker run \\"
     echo "    -e ServicePrincipalTenant=<valor> \\"
@@ -36,34 +43,48 @@ if [ -z "$ServicePrincipalId" ] && [ -z "$ServicePrincipalPass" ] && [ -z "$Serv
     echo "    <nome_da_imagem_docker>"
     exit 1
 fi
+
+# Autenticação com a Azure
+# Authentication with Azure
 echo "Estabelecendo autenticação com a Azure..."
+# Establishing authentication with Azure
 az login --service-principal -u "$ServicePrincipalId" -p "$ServicePrincipalPass" --tenant "$ServicePrincipalTenant"
 
+# Define a assinatura se especificada
+# Sets the subscription if specified
 if [ -n "$SubscriptionName" ]; then
     echo "Definindo a assinatura para: $SubscriptionName"
+    # Setting the subscription to: $SubscriptionName
     az account set --subscription "$SubscriptionName"
 fi
 
+# Lista e verifica os repositórios no registro Azure
+# Lists and checks the repositories in the Azure registry
 echo "Verificando o registro: $AzureRegistryName"
+# Checking the registry: $AzureRegistryName
 RepoList=($(az acr repository list --name "$AzureRegistryName" --output tsv))
 for RepositoryName in "${RepoList[@]}"; do
     echo "Verificando o repositório: $RepositoryName"
+    # Checking the repository: $RepositoryName
     RepositoryTags=$(az acr repository show-tags --name "$AzureRegistryName" --repository "$RepositoryName" --orderby time_desc --output tsv)
 
-    # Excluir por contagem se o usuário especificou NoOfKeptImages
+    # Excluir por contagem se NoOfKeptImages for especificado
+    # Delete by count if NoOfKeptImages is specified
     if [ "$NoOfKeptImages" -gt 0 ]; then
-        echo "IF NO OF KEPT IMAGES"
-
         count=0
         for tag in $RepositoryTags; do
             RepositoryTagName=$(echo "$tag" | awk -F_ '{print $NF}' | awk -F. '{print $1}')
 
+            # Ignora tags "latest" e aquelas contendo "migration"
+            # Skip "latest" tags and those containing "migration"
             if [ "$RepositoryTagName" = "latest" ] || [[ "$RepositoryTagName" == *"migration"* ]]; then
                 echo "Skipping tag: $RepositoryTagName"
                 echo "Skipping image: $RepositoryName/$tag"
                 continue
             fi
 
+            # Remove imagens além do número especificado em NoOfKeptImages
+            # Remove images beyond the number specified in NoOfKeptImages
             if [ $count -ge $NoOfKeptImages ]; then
                 ImageName="$RepositoryName:$tag"
                 remove_image "$AzureRegistryName" "$ImageName" "$DryRun"
@@ -74,12 +95,16 @@ for RepositoryName in "${RepoList[@]}"; do
         for tag in $RepositoryTags; do
             RepositoryTagName=$(echo "$tag" | awk -F_ '{print $NF}' | awk -F. '{print $1}')
             
+            # Ignora tags "latest" e "migration-latest"
+            # Skip "latest" and "migration-latest" tags
             if [ "$RepositoryTagName" == "latest" ] || [  "$RepositoryTagName" == "migration-latest" ]; then
                 echo "Skipping tag latest"
                 echo "Skipping image: $RepositoryName/latest"
                 continue
             fi
 
+            # Converte a tag para a data e compara com a data atual menos NoOfDays
+            # Convert the tag to date and compare it with the current date minus NoOfDays
             RepositoryTagBuildDay=$(date -d "$RepositoryTagName" "+%Y%m%d")
             ImageName="$RepositoryName:$tag"
 
@@ -94,7 +119,11 @@ for RepositoryName in "${RepoList[@]}"; do
     ((index++))
 done
 
+# Encerra a sessão da Azure
+# Ends the Azure session
 echo "Encerrando a sessão da Azure"
+# Ending the Azure session
 az logout
 
 echo "Execução do script concluída"
+# Script execution completed
